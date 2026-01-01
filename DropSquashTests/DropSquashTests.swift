36771b1feeb419619fc9eebb8e480ac4b31d6b70
echohuiecho <hiechohui@gmail.com>
2026-01-02 00:40:02 +0800

    Initial Commit

---
+++ DropSquashTests/DropSquashTests.swift
@@ -0,0 +1,17 @@
+//
+//  DropSquashTests.swift
+//  DropSquashTests
+//
+//  Created by 0 on 2/1/2026.
+//
+
+import Testing
+@testable import DropSquash
+
+struct DropSquashTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
